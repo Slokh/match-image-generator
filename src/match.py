c330dbc2e701f90a349b2a509d451a7b6c452fd8
KartikPatel6 <kp605@scarletmail.rutgers.edu>
2017-03-19 00:22:49 -0400

    Added EU Support

--- src/match.py
+++ src/match.py
@@ -15,19 +15,6 @@ score2 = scores.setdefault(sys.argv[4])
 team2 = teams.setdefault(sys.argv[5].lower())
 
 if tourney is not None and team1 is not None and team2 is not None and score1 is not None and score2 is not None:
-	dir = os.path.dirname(os.path.realpath(__file__))
-	image = Image.open(dir+"/../imgs/bg.png")
-
-	logo = Image.open(dir+"/../imgs/tourney-logos/"+tourney+".png").convert("RGBA")
-	logo.thumbnail((140,140))
-
-	team1_nameplate = Image.open(dir+"/../imgs/team-nameplates/"+team1+".png")
-	team1_logo = Image.open(dir+"/../imgs/team-logos/"+team1+".png")
-	team1_score = Image.open(dir+"/../imgs/score-numbers/"+score1+".png")
-
-	team2_nameplate = Image.open(dir+"/../imgs/team-nameplates/"+team2+".png")
-	team2_logo = Image.open(dir+"/../imgs/team-logos/"+team2+".png")
-	team2_score = Image.open(dir+"/../imgs/score-numbers/"+score2+".png")
 
 	tourney_width = (1024 - 140) / 2
 	tourney_height = 10
@@ -40,15 +27,28 @@ if tourney is not None and team1 is not None and team2 is not None and score1 is
 	score_width = (1024 - 62) / 2
 	score_height = 217
 
+	dir = os.path.dirname(os.path.realpath(__file__))
+	image = Image.open(dir+"/../imgs/bg.png")
+	
+	logo = Image.open(dir+"/../imgs/tourney-logos/"+tourney+".png").convert("RGBA")
+	logo.thumbnail((140,140))
 	image.paste(logo, (tourney_width, tourney_height), mask=logo) 
 
+	team1_nameplate = Image.open(dir+"/../imgs/team-nameplates/"+team1+".png")
+	team2_nameplate = Image.open(dir+"/../imgs/team-nameplates/"+team2+".png")
 	image.paste(team1_nameplate, (center_width-216, nameplate_height), mask=team1_nameplate) 
-	image.paste(team1_logo, (center_width-216, logo_height), mask=team1_logo) 
-	image.paste(team1_score, (score_width-437, score_height), mask=team1_score) 
-
 	image.paste(team2_nameplate, (center_width+216, nameplate_height), mask=team2_nameplate) 
+	
+	team1_logo = Image.open(dir+"/../imgs/team-logos/"+team1+".png")
+	team2_logo = Image.open(dir+"/../imgs/team-logos/"+team2+".png")
+	image.paste(team1_logo, (center_width-216, logo_height), mask=team1_logo) 
 	image.paste(team2_logo, (center_width+216, logo_height), mask=team2_logo) 
-	image.paste(team2_score, (score_width+437, score_height), mask=team2_score) 
+	
+	if score1 != "0" and score2 != "0":
+		team1_score = Image.open(dir+"/../imgs/score-numbers/"+score1+".png")
+		team2_score = Image.open(dir+"/../imgs/score-numbers/"+score2+".png")
+		image.paste(team1_score, (score_width-437, score_height), mask=team1_score) 
+		image.paste(team2_score, (score_width+437, score_height), mask=team2_score) 
 
 	image.save(dir+"/../output.png","PNG")
 else:
