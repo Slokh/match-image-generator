a735a90c3fe43fbd939bdb4180e6a2745902611f
KartikPatel6 <kp605@scarletmail.rutgers.edu>
2017-06-01 12:41:55 -0400

    Updated for Season 3 World Championship

--- src/match.py
+++ src/match.py
@@ -4,10 +4,11 @@ import sys
 
 from PIL import Image
 
-tourneys = {'rlcs':'RLCS'}
-teams = {'atelier':'Atelier', 'at':'Atelier', 'denial':'Denial Esports', 'den':'Denial Esports', 'g2':'G2 Esports', 'gen':'Genesis', 'genesis':'Genesis', 'nrg':'NRG Esports', 'rad':'Radiance', 'radiance':'Radiance', 'selfless':'Selfless Gaming', 'self':'Selfless Gaming', 't3':'Take 3', 'take3': 'Take 3', 'cow':'Cow Nose', 'cn':'Cow Nose', 'fs3':'FlipSid3 Tactics', 'flipsid3':'FlipSid3 Tactics', 'mock':'Mock-It eSports', 'mock-it':'Mock-It eSports', 'northern':'Northern Gaming', 'ng':'Northern Gaming', 'penta':'PENTA Sports', 'aces':'Pocket Aces', 'pa':'Pocket Aces', 'pocket':'Pocket Aces', 'sec':'Team Secrecy', 'secrecy':'Team Secrecy', 'leftovers':'The Leftovers', 'lo':'The Leftovers'}
+tourneys = {'rlcs':'RLCS', 'rlcsx':'RLCS'}
+teams = {'rogue':'Rogue', 'rge':'Rogue', 'denial':'Denial Esports', 'den':'Denial Esports', 'dnl':'Denial Esports', 'nrg':'NRG Esports', 'selfless':'Selfless Gaming', 'self':'Selfless Gaming', 'f3':'FlipSid3 Tactics', 'fs3':'FlipSid3 Tactics', 'flipsid3':'FlipSid3 Tactics', 'mock':'Mock-It eSports', 'mock-it':'Mock-It eSports', 'northern':'Northern Gaming', 'ng':'Northern Gaming', 'leftovers':'The Leftovers', 'lo':'The Leftovers', 'as':'Alpha Sydney', 'alpha':'Alpha Sydney', 'jam': 'JAM Gaming'}
 scores = {'0':'0', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5'}
 
+meta = sys.argv[1].lower();
 tourney = tourneys.setdefault(sys.argv[1].lower())
 team1 = teams.setdefault(sys.argv[2].lower())
 score1 = scores.setdefault(sys.argv[3])
@@ -16,15 +17,15 @@ team2 = teams.setdefault(sys.argv[5].lower())
 
 if tourney is not None and team1 is not None and team2 is not None and score1 is not None and score2 is not None:
 
-	tourney_width = (1024 - 140) / 2
+	tourney_width = int((1024 - 140) / 2)
 	tourney_height = 10
 
-	center_width = (1024 - 430) / 2
+	center_width = int((1024 - 430) / 2)
 
 	logo_height = 73
 	nameplate_height = 438
 
-	score_width = (1024 - 62) / 2
+	score_width = int((1024 - 62) / 2)
 	score_height = 217
 
 	dir = os.path.dirname(os.path.realpath(__file__))
@@ -44,7 +45,7 @@ if tourney is not None and team1 is not None and team2 is not None and score1 is
 	image.paste(team1_logo, (center_width-216, logo_height), mask=team1_logo) 
 	image.paste(team2_logo, (center_width+216, logo_height), mask=team2_logo) 
 	
-	if score1 != "0" and score2 != "0":
+	if meta != 'rlcsx':
 		team1_score = Image.open(dir+"/../imgs/score-numbers/"+score1+".png")
 		team2_score = Image.open(dir+"/../imgs/score-numbers/"+score2+".png")
 		image.paste(team1_score, (score_width-437, score_height), mask=team1_score) 
@@ -52,4 +53,4 @@ if tourney is not None and team1 is not None and team2 is not None and score1 is
 
 	image.save(dir+"/../output.png","PNG")
 else:
-	print "Please use the format: python match.py [tourney] [team1] [score1] [score2] [team2]"
+	print("Please use the format: python match.py [tourney] [team1] [score1] [score2] [team2]")
